023485b88184ae85cf4fafe30f144ade655f6cf0
muskan101v <varshney.muskan1998@gmail.com>
2024-12-08 21:49:48 +0530

    fix

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -27,7 +27,7 @@ export class FakeBackendInterceptor implements HttpInterceptor {
 
     return of(null)
       .pipe(mergeMap(handleRoute))
-      .pipe(materialize()) // call materialize and dematerialize to ensure delay even if an error is thrown (https://github.com/Reactive-Extensions/RxJS/issues/648)
+      .pipe(materialize())
       .pipe(delay(500))
       .pipe(dematerialize());
 
